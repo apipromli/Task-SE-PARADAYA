2a5c1ea4bd0c49c0ddfbae119af76226c4291ce0
apipromli <apipromli08@gmail.com>
2025-12-05 14:00:39 +0700

    tugas

---
+++ PalindromeChecker/Program.cs
@@ -0,0 +1,13 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+Console.WriteLine("Masukkan kata apakah kata tersebut termasuk plindrome:");
+string input = Console.ReadLine();
+string reversed = new string(input.Reverse().ToArray());
+if (input.Equals(reversed, StringComparison.OrdinalIgnoreCase))
+{
+    Console.WriteLine($"{input} adalah palindrome.");
+}
+else
+{
+    Console.WriteLine($"{input} bukan palindrome.");
+}
---
+++ TaskManagerCLI/Program.cs
@@ -0,0 +1,41 @@
+﻿using TaskManagerCLI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        List<TaskItem> tasks = new();
+        int id = 1;
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("1. Add | 2. List | 3. Complete | 4. Exit");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Title:");
+                    tasks.Add(new TaskItem
+                    {
+                        Id = id++,
+                        Title = Console.ReadLine()
+                    });
+                    break;
+                case "2":
+                    foreach (var t in tasks)
+                        Console.WriteLine($"{t.Id}. {t.Title} - {(t.IsCompleted ? "Done" : "Pending")} ");
+                    break;
+                case "3":
+                    Console.Write("Enter ID: ");
+                    int markId = int.Parse(Console.ReadLine());
+                    tasks.Find(t => t.Id == markId)?.MarkCompleted();
+                    break;
+                case "4":
+                    running = false; break;
+            }
+        }
+    }
+}
